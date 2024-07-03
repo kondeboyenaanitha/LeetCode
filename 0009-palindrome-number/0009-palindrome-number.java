164c857ec564463a5201a51236a27ea03b29262a
Anitha Kondeboyena <113623211+kondeboyenaanitha@users.noreply.github.com>
2024-07-03 12:00:52 +0530

    Time: 5 ms (72.34%), Space: 43.8 MB (65.58%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int rev=0;
+        int copy=x;
+        while(x>0){
+            rev=(rev*10)+(x%10);
+            x/=10;
+        }
+        return rev==copy;
+        
+    }
+}
